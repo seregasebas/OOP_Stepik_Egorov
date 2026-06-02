13db1bdf5afa864fb079a136a4c96ef142baad07
Serega <seregasebas@gmail.com>
2026-06-02 13:50:25 +0300

    from pc

--- start_oop.py
+++ start_oop.py
@@ -1,4 +1,33 @@
 print('Hello World!')
-print("Let's Go!")
-print("Let's Go!")
-print("start!")
+'--------------------'
+
+# class Person:
+#     name = "John Smith"
+#     age = 30
+#     gender = "male"
+#     address = "123 Main St"
+#     phone_number = "555-555-5555"
+#     email = "johnsmith@example.com"
+#     is_employed = True
+#
+# words = list(map(str, input().split()))
+#
+# for word in words:
+#     if hasattr(Person, word.lower()):
+#         print(f'{word}-YES')
+#     else:
+#         print(f'{word}-NO')
+'-------------------------------------------------------'
+# def is_obj_has_attr(obj: object, attr: str) -> bool:
+#     return hasattr(obj, attr)
+'-------------------------------------------------------'
+# class Robot:
+#     material = 'steel'
+#
+# print(getattr(Robot, 'steel'))
+#
+# class Robot:
+#     material = 'steel'
+#
+# print(getattr(Robot, 'steel', 'iron'))
+
