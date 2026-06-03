c7759d35b44651d9cdf986fe170468c4968ca15e
Sergey <seregasebas@gmail.com>
2026-06-03 16:52:32 +0700

    from mac

--- start_oop.py
+++ start_oop.py
@@ -1,6 +1,47 @@
+from django.conf import settings
+
 print('Hello World!')
 '--------------------'
-
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+# class Config:
+#     pass
+#
+# def create_instance(n: int) -> Config:
+#     ek = Config()
+#     for i in range(1, n+1):
+#         setattr(ek, f'attribute{i}', f'value{i}')
+#     return ek
+#
+# config_4 = create_instance(4)
+# config_3 = create_instance(3)
+# config_2 = create_instance(2)
+# print(config_4.__dict__)
+# print(config_3.__dict__)
+# print(config_2.__dict__)
+'-------------------------------------------------------'
+# class SuperHero:
+#     pass
+#
+# batman, superman = SuperHero(), SuperHero()
+#
+# batman.can_fly = True
+# setattr(batman, 'damage', 175)
+#
+# superman.can_fly = True
+# setattr(superman, 'damage', 275)
+# setattr(superman, 'alter_ego', 'Кларк Кент')
+#
+# print(f'{superman.__dict__=}, {batman.__dict__=}')
+'-------------------------------------------------------'
 # class Person:
 #     name = "John Smith"
 #     age = 30
