f2998c885bc2a7e99220cba9e7bf9b27c51ccc46
Serega <seregasebas@gmail.com>
2026-06-15 12:09:47 +0300

    from PC

--- start_oop.py
+++ start_oop.py
@@ -13,7 +13,37 @@ print('Hello World!')
 '-------------------------------------------------------'
 '-------------------------------------------------------'
 '-------------------------------------------------------'
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def greet(self):
+#         return f"Hello, my name is {self.name}, and I am {self.age} years old"
+#
+#
+# # Ниже расположен код для проверок, его не нужно удалять
+# bro = Person('Nikolay', 34)
+# assert bro.age == 34
+# assert bro.name == 'Nikolay'
+# assert bro.greet() == 'Hello, my name is Nikolay, and I am 34 years old'
+#
+# sister = Person('Elena', 21)
+# assert sister.age == 21
+# assert sister.name == 'Elena'
+# assert sister.greet() == 'Hello, my name is Elena, and I am 21 years old'
+# print('Good')
 '-------------------------------------------------------'
+# class Vehicle():
+#
+#     def __init__(self, max_speed, mileage):
+#         self.max_speed = max_speed
+#         self.mileage = mileage
+#
+# bmw = Vehicle(240, 18)
+# audi = Vehicle(123, 43)
+# print(bmw.__dict__)
+# print(audi.__dict__)
 '-------------------------------------------------------'
 # class Point:
 #     def set_coordinates(self, x, y):
