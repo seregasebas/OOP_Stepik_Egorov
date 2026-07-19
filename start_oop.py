035e0dc8e76844cb151de0cdb61924abe54da524
Serega <seregasebas@gmail.com>
2026-07-19 16:25:42 +0300

    from PC

--- start_oop.py
+++ start_oop.py
@@ -1,8 +1,7 @@
 from functools import wraps
 import time
 from datetime import datetime
-import json
-
+from string import ascii_letters
 print('Hello World!')
 '--------------------'
 '-------------------------------------------------------'
@@ -13,6 +12,348 @@ print('Hello World!')
 '-------------------------------------------------------'
 '-------------------------------------------------------'
 '-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+# class File:
+#
+#     def __init__(self, name):
+#         self.name = name
+#         self.in_trash = False
+#         self.is_deleted = False
+#
+#     def restore_from_trash(self):
+#         print(f'Файл {self.name} восстановлен из корзины')
+#         self.in_trash = False
+#
+#     def remove(self):
+#         print(f'Файл {self.name} был удален')
+#         self.is_deleted = True
+#
+#     def read(self):
+#         if self.is_deleted:
+#             print(f'ErrorReadFileDeleted({self.name})')
+#             return None
+#         elif self.in_trash:
+#             print(f'ErrorReadFileTrashed({self.name})')
+#             return None
+#
+#         else:
+#             print(f'Прочитали все содержимое файла {self.name}')
+#
+#     def write(self, content):
+#         if self.in_trash:
+#             print(f'ErrorWriteFileTrashed({self.name})')
+#             return None
+#         if self.is_deleted:
+#             print(f'ErrorWriteFileDeleted({self.name})')
+#             return None
+#         else:
+#             print(f'Записали значение {content} в файл {self.name}')
+#
+# class Trash:
+#     content = []
+#
+#     @staticmethod
+#     def add(file):
+#         if not isinstance(file, File):
+#             print(f'В корзину можно добавлять только файл')
+#             return None
+#         else:
+#             Trash.content.append(file)
+#             file.in_trash = True
+#             return None
+#
+#     @staticmethod
+#     def clear():
+#         print('Очищаем корзину')
+#         for tr in Trash.content:
+#             tr.remove()
+#         Trash.content = []
+#         print('Корзина пуста')
+#
+#     @staticmethod
+#     def restore():
+#         print('Восстанавливаем файлы из корзины')
+#         for tr in Trash.content:
+#             tr.restore_from_trash()
+#         Trash.content = []
+#         print('Корзина пуста')
+#
+# f1 = File('puppies.jpg')
+# f2 = File('cat.jpg')
+# f3 = File('xxx.doc')
+# passwords = File('pass.txt')
+#
+# for file in [f1, f2, f3, passwords]:
+#     assert file.is_deleted is False
+#     assert file.in_trash is False
+#
+# f3.read()
+# f3.remove()
+# assert f3.is_deleted is True
+# f3.read()
+# f3.write('hello')
+#
+# assert Trash.content == []
+#
+# Trash.add(f2)
+# Trash.add(passwords)
+# Trash.add(f3)
+#
+# f1.read()
+# Trash.add(f1)
+# f1.read()
+#
+# for file in [f1, f2, f3, passwords]:
+#     assert file.in_trash is True
+#
+# for f in [f2, passwords, f3, f1]:
+#     assert f in Trash.content
+#
+# Trash.restore()
+# assert Trash.content == [], 'После восстановления корзина должна была очиститься'
+#
+# Trash.add(passwords)
+# Trash.add(f2)
+# Trash.add('hello')
+# Trash.add(f1)
+# print(f'{f1.in_trash=}')
+#
+# for f in [passwords, f2, f1]:
+#     assert f in Trash.content
+#
+#
+# Trash.clear()
+#
+# for file in [passwords, f2, f1]:
+#     assert file.is_deleted is True
+#
+# assert Trash.content == [], 'После удаления файлов корзина должна была очиститься'
+# print('Here')
+# f1.read()
+#
+# # # Ниже код для проверки класса File
+# # f1 = File('puppies.jpg')
+# # assert f1.name == 'puppies.jpg'
+# # assert f1.in_trash is False
+# # assert f1.is_deleted is False
+# #
+# # f1.read()  # Прочитали все содержимое файла puppies.jpg
+# # f1.remove()  # Файл puppies.jpg был удален
+# # assert f1.is_deleted is True
+# # f1.read()  # ErrorReadFileDeleted(puppies.jpg)
+# #
+# # passwords = File('pass.txt')
+# # assert passwords.name == 'pass.txt'
+# # assert passwords.in_trash is False
+# # assert passwords.is_deleted is False
+# #
+# # f3 = File('xxx.doc')
+# #
+# # assert f3.__dict__ == {'name': 'xxx.doc', 'in_trash': False, 'is_deleted': False}
+# # f3.read()
+# # f3.remove()
+# # assert f3.is_deleted is True
+# # f3.read()
+# # f3.in_trash = True
+# # f3.is_deleted = False
+# # f3.read()
+# # f3.write('hello')
+# # f3.restore_from_trash()
+# # assert f3.in_trash is False
+# # f3.write('hello')  # Записали значение «hello» в файл cat.jpg
+# #
+# # f2 = File('cat.jpg')
+# # f2.write('hello')  # Записали значение «hello» в файл cat.jpg
+# # f2.write([1, 2, 3])  # Записали значение «hello» в файл cat.jpg
+# # f2.remove()  # Файл cat.jpg был удален
+# # f2.write('world')  # ErrorWriteFileDeleted(cat.jpg)
+'-------------------------------------------------------'
+# class Registration:
+#     @staticmethod
+#     def is_include_digit(password):
+#         if not any(ch.isdigit() for ch in password):
+#             raise ValueError('Пароль должен содержать хотя бы одну цифру')
+#     @staticmethod
+#     def is_include_all_register(password):
+#         if not any((ch.isupper() for ch in password) and (ch.islower() for ch in password)):
+#             raise ValueError('Пароль должен содержать хотя бы один символ верхнего и нижнего регистра')
+#     @staticmethod
+#     def is_include_only_latin(password):
+#         for ch in password:
+#             if not ch.isdigit():
+#                 if ch not in ascii_letters:
+#                     raise ValueError('Пароль должен содержать только латинский алфавит')
+#     @staticmethod
+#     def check_password_dictionary(password):
+#         with open('txt/easy_passwords.txt', 'r', encoding='utf-8') as f:
+#             return password in f.read()
+#
+#     @property
+#     def login(self):
+#         return self.__login
+#     @login.setter
+#     def login(self, login):
+#         if not isinstance(login, str):
+#             raise TypeError
+#         if '@' not in login and login.count('@') !=1:
+#             raise ValueError
+#         else:
+#             if '.' not in login.split('@')[1]:
+#                 raise ValueError
+#             self.__login = login
+#
+#     @property
+#     def password(self):
+#         return self.__password
+#     @password.setter
+#     def password(self, password: str):
+#         if not isinstance(password, str):
+#             raise TypeError("Пароль должен быть строкой")
+#         if not 4 < len(password) < 12:
+#             raise ValueError('Пароль должен быть длиннее 4 и меньше 12 символов')
+#         if Registration.check_password_dictionary(password):
+#             raise ValueError('Ваш пароль содержится в списке самых легких')
+#         if (not Registration.is_include_digit(password)
+#                 and not Registration.is_include_all_register(password)
+#                 and not Registration.is_include_only_latin(password)):
+#             self.__password = password
+#
+#     def __init__(self, login, password):
+#         self.login = login
+#         self.password = password
+#
+# try:
+#     s2 = Registration("fga", "asd12")
+# except ValueError as e:
+#     pass
+# else:
+#     raise ValueError("Registration('fga', 'asd12') как можно записать такой логин?")
+#
+# try:
+#     s2 = Registration("fg@a", "asd12")
+# except ValueError as e:
+#     pass
+# else:
+#     raise ValueError("Registration('fg@a', 'asd12') как можно записать такой логин?")
+#
+# s2 = Registration("translate@gmail.com", "as1SNdf")
+# try:
+#     s2.login = "asdsa12asd."
+# except ValueError as e:
+#     pass
+# else:
+#     raise ValueError("asdsa12asd как можно записать такой логин?")
+#
+# try:
+#     s2.login = "asdsa12@asd"
+# except ValueError as e:
+#     pass
+# else:
+#     raise ValueError("asdsa12@asd как можно записать такой логин?")
+#
+# assert Registration.check_password_dictionary('QwerTy123'), 'проверка на пароль в слове не работает'
+#
+# try:
+#     s2.password = "QwerTy123"
+# except ValueError as e:
+#     pass
+# else:
+#     raise ValueError("QwerTy123 хранится в словаре паролей, как его можно было сохранить?")
+#
+#
+# try:
+#     s2.password = "KissasSAd1f"
+# except ValueError as e:
+#     pass
+# else:
+#     raise ValueError("KissasSAd1f хранится в словаре паролей, как его можно было сохранить?")
+#
+# try:
+#     s2.password = "124244242"
+# except ValueError as e:
+#     pass
+# else:
+#     raise ValueError("124244242 пароль НЕОЧЕНЬ, как его можно было сохранить?")
+#
+# try:
+#     s2.password = "RYIWUhjkdbfjfgdsffds"
+# except ValueError as e:
+#     pass
+# else:
+#     raise ValueError("RYIWUhjkdbfjfgdsffds пароль НЕОЧЕНЬ, как его можно было сохранить?")
+#
+# try:
+#     s2.password = "CaT"
+# except ValueError as e:
+#     pass
+# else:
+#     raise ValueError("CaT пароль НЕОЧЕНЬ, как его можно было сохранить?")
+#
+# try:
+#     s2.password = "monkey"
+# except ValueError as e:
+#     pass
+# else:
+#     raise ValueError("monkey пароль НЕОЧЕНЬ, как его можно было сохранить?")
+#
+# try:
+#     s2.password = "QwerTy123"
+# except ValueError as e:
+#     pass
+# else:
+#     raise ValueError("QwerTy123 пароль есть в слове, нельзя его использовать")
+#
+# try:
+#     s2.password = "HelloQEWq"
+# except ValueError as e:
+#     pass
+# else:
+#     raise ValueError("HelloQEWq пароль НЕОЧЕНЬ, как его можно было сохранить?")
+#
+# try:
+#     s2.password = [4, 32]
+# except TypeError as e:
+#     pass
+# else:
+#     raise TypeError("Пароль должен быть строкой")
+#
+# try:
+#     s2.password = 123456
+# except TypeError as e:
+#     pass
+# else:
+#     raise TypeError("Пароль должен быть строкой")
+#
+# print('U r hacked Pentagon')
+'-------------------------------------------------------'
+# class Pizza:
+#     def __init__(self, ingredients=None):
+#         if ingredients is None:
+#             ingredients = []
+#         self.ingredients = ingredients
+#
+#     @classmethod
+#     def margherita(cls):
+#         return cls(ingredients=['mozzarella', 'tomatoes'])
+#     @classmethod
+#     def peperoni(cls):
+#         return cls(ingredients=['mozzarella', 'peperoni', 'tomatoes'])
+#     @classmethod
+#     def barbecue(cls):
+#         return cls(ingredients=['mozzarella', 'red onion', 'sauce bbq', 'chicken'])
+# bbq = Pizza.barbecue()
+# peperoni = Pizza.peperoni()
+# margherita = Pizza.margherita()
+# print(sorted(bbq.ingredients))
+# print(sorted(peperoni.ingredients))
+# print(sorted(margherita.ingredients))
 '-------------------------------------------------------'
 # class BankAccount:
 #     bank_name = "Tinkoff Bank"
@@ -29,18 +370,18 @@ print('Hello World!')
 #     @classmethod
 #     def bank_info(cls):
 #         return f"{cls.bank_name} is located in {cls.address}"
-
-oleg = BankAccount.create_account("Олег Тинкофф", 1000)
-assert isinstance(oleg, BankAccount)
-assert oleg.name == 'Олег Тинкофф'
-assert oleg.balance == 1000
-assert BankAccount.bank_info() == 'Tinkoff Bank is located in Москва, ул. 2-я Хуторская, д. 38А'
-
-ivan = BankAccount.create_account("Ivan Reon", 50)
-assert isinstance(ivan, BankAccount)
-assert ivan.name == 'Ivan Reon'
-assert ivan.balance == 50
-print('Good')
+#
+# oleg = BankAccount.create_account("Олег Тинкофф", 1000)
+# assert isinstance(oleg, BankAccount)
+# assert oleg.name == 'Олег Тинкофф'
+# assert oleg.balance == 1000
+# assert BankAccount.bank_info() == 'Tinkoff Bank is located in Москва, ул. 2-я Хуторская, д. 38А'
+#
+# ivan = BankAccount.create_account("Ivan Reon", 50)
+# assert isinstance(ivan, BankAccount)
+# assert ivan.name == 'Ivan Reon'
+# assert ivan.balance == 50
+# print('Good')
 '-------------------------------------------------------'
 # class User:
 #
