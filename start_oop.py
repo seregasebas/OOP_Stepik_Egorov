ec6349013f0257a2f6044d91b889cbde92e1a03a
Sergey <seregasebas@gmail.com>
2026-07-22 18:07:31 +0700

    from Mac

--- start_oop.py
+++ start_oop.py
@@ -18,6 +18,118 @@ print('Hello World!')
 '-------------------------------------------------------'
 '-------------------------------------------------------'
 '-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+'-------------------------------------------------------'
+# class Hero:
+#
+#     def __len__(self):
+#         return len(self.__dict__.keys())
+#
+#     def __str__(self):
+#         res = ''
+#         for key, value in sorted(self.__dict__.items(), key=lambda x: x[0]):
+#             res = res + f'{key}: {value}\n'
+#         return res.rstrip('\n')
+#
+# # Ниже код для проверки методов класса Hero
+# hero = Hero()
+# assert len(hero) == 0
+# hero.health = 50
+# hero.damage = 10
+# assert len(hero) == 2
+# print(hero)
+# assert str(hero) == '''damage: 10
+# health: 50'''
+# hero.weapon = ['sword', ' bow']
+# hero.skill = 'Некромант'
+# assert str(hero) == '''damage: 10
+# health: 50
+# skill: Некромант
+# weapon: ['sword', ' bow']'''
+# print(hero)
+#
+# villain = Hero()
+# assert str(villain) == ''
+# assert len(villain) == 0
+# villain.level = 15
+# villain.skill = 'Боец'
+# villain.armor = 25
+# assert len(villain) == 3
+# assert str(villain) == '''armor: 25
+# level: 15
+# skill: Боец'''
+# print(villain)
+'-------------------------------------------------------'
+# text = "Hello, world!"
+# print(text.__dir__())
+'-------------------------------------------------------'
+# class Ingredient:
+#     def __init__(self, name, weight):
+#         self.name = name
+#         self.weight = weight
+#
+#     def __str__(self):
+#         return f'{self.name}: {self.weight}г.'
+#
+#
+# class Pizza:
+#     def __init__(self, name, ingredients=None):
+#         self.name = name
+#         if ingredients is None:
+#             self.ingredients = []
+#         else:
+#             self.ingredients = ingredients
+#
+#     def __str__(self):
+#         igr = '\n'.join(map(str, sorted(self.ingredients, key = lambda x: x.weight, reverse = True)))
+#         return (f'Пицца {self.name} состоит из:\n'
+#                 f'{igr}')
+#
+# tomatoes = Ingredient('tomatoes', 200)
+# cheese = Ingredient('mozzarella', 400)
+# # print(tomatoes)
+# # print(cheese)
+#
+# peperoni = Pizza('Пеперони')
+# peperoni.ingredients.append(tomatoes)
+# peperoni.ingredients.append(cheese)
+# print(peperoni)
+#
+# barbecue = Pizza('BBQ', [
+#     Ingredient('chicken', 200),
+#     Ingredient('mozzarella', 300),
+#     Ingredient('sauce bbq', 150),
+#     Ingredient('red onion', 150)
+# ])
+#
+# print(barbecue)
+'-------------------------------------------------------'
+# class GroceryItem:
+#
+#     def __init__(self, name, price, quantity):
+#         self.name = name
+#         self.price = price
+#         self.quantity = quantity
+#
+#     def __str__(self):
+#         return f'Name: {self.name}\nPrice: {self.price}\nQuantity: {self.quantity}'
+#
+#     def __repr__(self):
+#         return f'GroceryItem({self.name}, {self.price}, {self.quantity})'
+#
+# banana = GroceryItem('Banana', 10, 5)
+# print(banana)
+# print(f"{banana!r}")
+#
+# dragon_fruit = GroceryItem('Dragon fruit', 5, 350)
+# print(str(dragon_fruit))
+# print(repr(dragon_fruit))
+'-------------------------------------------------------'
 # class Vector:
 #
 #     def __init__(self, *args):
