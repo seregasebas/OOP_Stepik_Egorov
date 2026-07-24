56a1ec3ed8665696173dbd28613ac6288bb55d59
Sergey <seregasebas@gmail.com>
2026-07-24 17:45:15 +0700

    from Mac

--- start_oop.py
+++ start_oop.py
@@ -22,6 +22,63 @@ print('Hello World!')
 '-------------------------------------------------------'
 '-------------------------------------------------------'
 '-------------------------------------------------------'
+# class Vector:
+#
+#     def __init__(self, *args):
+#         # self.values = sorted([arg for arg in args if type(arg) == int])
+#         self.values = []
+#         if len(args) > 0:
+#             for arg in args:
+#                 if isinstance(arg, int):
+#                     self.values.append(arg)
+#         self.values.sort()
+#
+#     def __str__(self):
+#         if len(self.values) == 0:
+#             return 'Пустой вектор'
+#         else:
+#             return f"Вектор{tuple(val for val in self.values)}"
+#
+#     def __add__(self, other):
+#         if isinstance(other, int):
+#             res = [arg + other for arg in self.values]
+#             return Vector(*res)
+#         if isinstance(other, Vector) and len(self.values) == len(other.values):
+#             res = [se + ot for se, ot in zip(self.values, other.values)]
+#             return Vector(*res)
+#         elif isinstance(other, Vector) and len(self.values) != len(other.values):
+#             print('Сложение векторов разной длины недопустимо')
+#             return None
+#         else:
+#             print(f'Вектор нельзя сложить с {other}')
+#             return None
+#
+#     def __mul__(self, other):
+#         if isinstance(other, int):
+#             res = [arg * other for arg in self.values]
+#             return Vector(*res)
+#         if isinstance(other, Vector) and len(self.values) == len(other.values):
+#             res = [se * ot for se, ot in zip(self.values, other.values)]
+#             return Vector(*res)
+#         elif isinstance(other, Vector) and len(self.values) != len(other.values):
+#             print('Умножение векторов разной длины недопустимо')
+#             return None
+#         else:
+#             print(f'Вектор нельзя умножать с {other}')
+#             return None
+# v1 = Vector(1,2,3)
+# print(v1.values)
+# print(v1) # печатает "Вектор(1, 2, 3)"
+#
+# v2 = Vector(3,4,5)
+# print(v2) # печатает "Вектор(3, 4, 5)"
+# v3 = v1 + v2
+# print(v3) # печатает "Вектор(4, 6, 8)"
+# v4 = v3 + 5
+# print(v4) # печатает "Вектор(9, 11, 13)"
+# v5 = v1 * 2
+# print(v5) # печатает "Вектор(2, 4, 6)"
+# v5 + 'hi' # печатает "Вектор нельзя сложить с hi"
 '-------------------------------------------------------'
 # class Order:
 #
