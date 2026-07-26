fed4dd632a897a228b650f2a7f70f86a1037f5fc
Serega <seregasebas@gmail.com>
2026-07-26 16:23:01 +0300

    from PC

--- start_oop.py
+++ start_oop.py
@@ -1,8 +1,9 @@
-from collections import defaultdict
-from functools import wraps
-import time
-from datetime import datetime
-from string import ascii_letters
+# from collections import defaultdict
+# from functools import wraps
+# import time
+# from datetime import datetime
+# from string import ascii_letters
+# from functools import total_ordering
 print('Hello World!')
 '--------------------'
 '-------------------------------------------------------'
@@ -19,6 +20,59 @@ print('Hello World!')
 '-------------------------------------------------------'
 '-------------------------------------------------------'
 '-------------------------------------------------------'
+# @total_ordering
+# class Rectangle:
+#
+#     def __init__(self, width, height):
+#         self.width = width
+#         self.height = height
+#
+#     @property
+#     def area(self):
+#         return self.width * self.height
+#
+#     def __eq__(self, other):
+#         if isinstance(other, Rectangle):
+#             return self.area == other.area
+#         return self.area == other
+#
+#     def __lt__(self, other):
+#         if isinstance(other, Rectangle):
+#             return self.area < other.area
+#         return self.area < other
+#
+# r1 = Rectangle(3, 4)
+# assert r1.width == 3
+# assert r1.height == 4
+# assert r1.area == 12
+# print(f'{type(r1).area=}')
+# assert isinstance(type(r1).area, property), 'Вы не создали property area'
+#
+# assert r1 > 11
+# assert not r1 > 12
+# assert r1 >= 12
+# assert r1 <= 12
+# assert not r1 > 13
+# assert not r1 == 13
+# assert r1 != 13
+# assert r1 == 12
+#
+# r2 = Rectangle(2, 6)
+# assert r1 == r2
+# assert not r1 != r2
+# assert not r1 > r2
+# assert not r1 < r2
+# assert r1 >= r2
+# assert r1 <= r2
+#
+# r3 = Rectangle(5, 2)
+# assert not r2 == r3
+# assert r2 != r3
+# assert r2 > r3
+# assert not r2 < r3
+# assert r2 >= r3
+# assert not r2 <= r3
+# print('Good')
 '-------------------------------------------------------'
 # class ChessPlayer:
 #
