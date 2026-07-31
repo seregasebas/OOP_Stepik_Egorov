81dd0772a84475e818063049280bd0ad5f993f22
Serega <seregasebas@gmail.com>
2026-07-31 17:51:58 +0300

    from PC

--- start_oop.py
+++ start_oop.py
@@ -28,7 +28,136 @@ print('Hello World!')
 '-------------------------------------------------------'
 '-------------------------------------------------------'
 '-------------------------------------------------------'
+# class ShoppingCart:
+#
+#     def __init__(self):
+#         self.items = {}
+#
+#     def __getitem__(self, item):
+#         return self.items[item] if item in self.items else 0
+#
+#     def __setitem__(self, key, value):
+#         self.items[key] = value
+#
+#     def __delitem__(self, key):
+#         del self.items[key]
+#
+#     def add_item(self, name, value=1):
+#         self.items.setdefault(name, 0)
+#         self.items[name] += value
+#
+#     def remove_item(self, name, value=1):
+#         if name in self.items:
+#             if value >= self.items[name]:
+#                 del self.items[name]
+#             else:
+#                 self.items[name] -= value
+#
+# # Create a new shopping cart
+# cart = ShoppingCart()
+#
+# # Add some items to the cart
+# cart.add_item('Apple', 3)
+# cart.add_item('Banana', 2)
+# cart.add_item('Orange')
+#
+# assert cart['Banana'] == 2
+# assert cart['Orange'] == 1
+# assert cart['Kivi'] == 0
+#
+# cart.add_item('Orange', 9)
+# assert cart['Orange'] == 10
+#
+# print("Shopping Cart:")
+# for item_name in cart.items:
+#     print(f"{item_name}: {cart[item_name]}")
+#
+# cart['Apple'] = 5
+# print(cart['Apple'])
+# cart['Banana'] = 7
+# cart['Kivi'] = 11
+# assert cart['Apple'] == 5
+# assert cart['Banana'] == 7
+# assert cart['Kivi'] == 11
+#
+# print("Updated Shopping Cart:")
+# for item_name in cart.items:
+#     print(f"{item_name}: {cart[item_name]}")
+#
+# # Remove an item from the cart
+# cart.remove_item('Banana')
+# assert cart['Banana'] == 6
+#
+# cart.remove_item('Apple', 4)
+# assert cart['Apple'] == 1
+#
+# cart.remove_item('Apple', 2)
+# assert cart['Apple'] == 0
+# assert 'Apple' not in cart.items
+#
+# cart.remove_item('Potato')
+#
+# del cart['Banana']
+# assert cart['Banana'] == 0
+# assert 'Banana' not in cart.items
+#
+# print("Updated Shopping Cart:")
+# for item_name in cart.items:
+#     print(f"{item_name}: {cart[item_name]}")
+#
+# print('Good')
 '-------------------------------------------------------'
+# class Song:
+#
+#     def __init__(self,title, artist):
+#         self.title = title
+#         self.artist = artist
+#
+# class Playlist:
+#
+#     def __init__(self):
+#         self.songs = []
+#
+#     def __getitem__(self, item):
+#         return self.songs[item]
+#
+#     def __setitem__(self, key, value):
+#         self.songs.insert(key, value)
+#
+#     def add_song(self, song):
+#         self.songs.append(song)
+#
+#     # def __repr__(self):
+#     #     return f"{self.songs}"
+#     def __str__(self):
+#         res = []
+#         for song in self.songs:
+#             res.append(f'{song.title} - {song.artist}')
+#         return str(res)
+#
+#
+# playlist = Playlist()
+# assert len(playlist.songs) == 0
+# assert isinstance(playlist, Playlist)
+# playlist.add_song(Song("Paradise", "Coldplay"))
+#
+# assert playlist[0].title == 'Paradise'
+# assert playlist[0].artist == 'Coldplay'
+# assert len(playlist.songs) == 1
+#
+# playlist[0] = Song("Resistance", "Muse")
+# assert playlist[0].title == 'Resistance'
+# assert playlist[0].artist == 'Muse'
+# assert playlist[1].title == 'Paradise'
+# assert playlist[1].artist == 'Coldplay'
+# print(playlist)
+# playlist[1] = Song("Helena", "My Chemical Romance")
+# assert playlist[1].title == 'Helena'
+# assert playlist[1].artist == 'My Chemical Romance'
+#
+# assert playlist[2].title == 'Paradise'
+# assert playlist[2].artist == 'Coldplay'
+# print('Good')
 '-------------------------------------------------------'
 # class Building:
 #
