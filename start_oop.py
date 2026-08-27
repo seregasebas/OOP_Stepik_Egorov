24c05087b690e1d9b43fd302975e0d13a8ba39bd
Serega <seregasebas@gmail.com>
2026-08-27 11:28:12 +0300

    from PC

--- start_oop.py
+++ start_oop.py
@@ -24,10 +24,120 @@ print('Hello World!')
 '-------------------------------------------------------'
 '-------------------------------------------------------'
 '-------------------------------------------------------'
+# class First(object):
+#     def __init__(self):
+#         print("First(): entering")
+#         super(First, self).__init__()
+#         print("First(): exiting")
+#
+#
+# class Second(object):
+#     def __init__(self):
+#         print("Second(): entering")
+#         super(Second, self).__init__()
+#         print("Second(): exiting")
+#
+#
+# class Third(First, Second):
+#     def __init__(self):
+#         print("Third(): entering")
+#         super(Third, self).__init__()
+#         print("Third(): exiting")
+#
+# Third()
+#
+# print(Third.mro())
 '-------------------------------------------------------'
+# print(list.__mro__)
 '-------------------------------------------------------'
+# class Person:
+#
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def display_person_info(self):
+#         print(f"Person: {self.name}, {self.age}")
+#
+# class Company:
+#
+#     def __init__(self, company_name, location):
+#         self.company_name = company_name
+#         self.location = location
+#
+#     def display_company_info(self):
+#         print(f"Company: {self.company_name}, {self.location}")
+#
+# class Employee(Person, Company):
+#
+#     def __init__(self, name, age, company_name, location):
+#         super().__init__(name, age)
+#         Company.__init__(self, company_name, location)
+#
+# a = Person('Ivan', 32)
+# a.display_person_info()
+#
+# a = Company('Zara', 'Prague')
+# a.display_company_info()
+#
+# emp = Employee('Jessica', 28, 'Google', 'Atlanta')
+# emp.display_person_info()
+# emp.display_company_info()
+#
+# emp2 = Employee('Kolya', 11, 'Facebook', 'Seatle')
+# emp2.display_person_info()
+# emp2.display_company_info()
 '-------------------------------------------------------'
+# class User:
+#     def __init__(self, username, password):
+#         self.username = username
+#         self.password = password
+#
+#     def get_info(self):
+#         return f"Имя пользователя: {self.username}"
+#
+# class Authentication:
+#
+#     def authenticate(self, username, password):
+#         return self.username == username and self.password == password
+#
+# class AuthenticatedUser(Authentication, User):
+#     pass
+#
+# assert issubclass(AuthenticatedUser, User) is True
+# assert issubclass(AuthenticatedUser, Authentication) is True
+#
+# user1 = AuthenticatedUser('user1', 'password1')
+# assert user1.get_info() == 'Имя пользователя: user1'
+# assert user1.authenticate('user1', 'password2') is False
+# assert user1.authenticate('user1', 'password1') is True
+#
+# ted = AuthenticatedUser('ted_lawyer', 'alligator3')
+# print(ted.get_info())
+# print('GOOD')
 '-------------------------------------------------------'
+# class Mother:
+#     def __init__(self, mother_name):
+#         self.mother_name = mother_name
+#
+#
+# class Father:
+#     def __init__(self, father_name):
+#         self.father_name = father_name
+#
+#
+# class Child(Mother, Father):
+#     def __init__(self, child_name, mother_name, father_name):
+#         super().__init__(mother_name)
+#         Father.__init__(self, father_name)
+#         self.child_name = child_name
+#
+#     def introduce(self):
+#         return f"Меня зовут {self.child_name}. Моя мама - {self.mother_name}, мой папа - {self.father_name}"
+#
+#
+# child = Child("Анна", "Мария", "Иван")
+# print(child.introduce())
 '-------------------------------------------------------'
 # class Initialization:
 #
